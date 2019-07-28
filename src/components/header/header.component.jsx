979e7827d04d09c1c20817e55eca793ef062b527
Mike Nichols <mikenichols410@gmail.com>
2019-07-28 03:04:41 -0400

    Added cart button and show/hide functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,16 +4,20 @@ import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { auth } from '../../firebase/firebase.utills';
-
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
         </Link>
         <div className='options'>
+        <Link className='option' to='/shop'>
+                CONTACT
+            </Link>
             <Link className='option' to='/shop'>
                 SHOP
             </Link>
@@ -23,11 +27,17 @@ const Header = ({ currentUser }) => (
                     :
                     <Link className='option' to='/signin'>SIGN IN</Link>
                 }
+                <CartIcon />
         </div>
+        {
+            hidden ? null :
+        <CartDropdown />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header);
